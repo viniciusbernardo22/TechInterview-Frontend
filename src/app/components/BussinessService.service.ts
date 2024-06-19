7bb5fdac34c671fdba78e67ac58a839ed883a3d5
Vinicius <vini383@gmail.com>
2024-06-19 18:34:40 -0300

    fix: fixed providing bussinessService.service.ts on root to correctly

--- src/app/components/BussinessService.service.ts
+++ src/app/components/BussinessService.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from "@angular/core";
 import { timeout } from "rxjs";
 
-@Injectable()
+/* The service needs to be injected, or you will not be able to use it */
+@Injectable({
+  providedIn: 'root',
+})
+
 export class BussinessService {
     async getMessage(): Promise<string> {
         return new Promise<string>(resolve => {
             timeout(1000);
-            resolve("May the force be with you!");         
+            resolve("May the force be with you!");
         });
     }
 }
